1e8f105579fefb35ed64daa254ce9573a08c42d0
Mufeez08 <56508719+Mufeez08@users.noreply.github.com>
2022-07-16 17:16:26 -0400

    step-19

---
+++ lib/services/cloud/cloud_storage_exceptions.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+
+@immutable
+class CloudStorageException implements Exception {
+  const CloudStorageException();
+}
+
+// C in CRUD
+class CouldNoteCreateNoteException extends CloudStorageException {}
+
+// R in CRUD
+class CouldNotGetAllNotesException extends CloudStorageException {}
+
+// U in CRUD
+class CouldNoteUpdateNoteException extends CloudStorageException {}
+
+// D in CRUD
+class CouldNotDeleteNoteException extends CloudStorageException {}
