e1802aebf21a0ecb9674a833b779d5ee716e086f
Mufeez08 <56508719+Mufeez08@users.noreply.github.com>
2022-07-19 07:20:19 -0400

    step-23

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:orangenote/constants/routes.dart';
 import 'package:orangenote/services/auth/auth_exceptions.dart';
-import 'package:orangenote/services/auth/auth_service.dart';
+import 'package:orangenote/services/auth/bloc/auth_bloc.dart';
+import 'package:orangenote/services/auth/bloc/auth_event.dart';
 import 'package:orangenote/utilities/dialogs/error_dialog.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -61,22 +63,12 @@ class _LoginViewState extends State<LoginView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                await AuthService.firebase().logIn(
-                  email: email,
-                  password: password,
-                );
-                final user = AuthService.firebase().currentUser;
-                if (user?.isEmailVerified ?? false) {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    notesRoute,
-                    (route) => false,
-                  );
-                } else {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    verifyEmailRoute,
-                    (route) => false,
-                  );
-                }
+                context.read<AuthBloc>().add(
+                      AuthEventLogIn(
+                        email,
+                        password,
+                      ),
+                    );
               } on UserNotFoundAuthException {
                 await showErrorDialog(
                   context,
