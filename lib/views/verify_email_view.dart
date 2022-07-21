2dc621c6453a4048fd1b2233fbc976c1b3f54cc4
Mufeez08 <56508719+Mufeez08@users.noreply.github.com>
2022-07-21 07:55:08 -0400

    step-25

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:orangenote/constants/routes.dart';
 import 'package:orangenote/services/auth/auth_service.dart';
+import 'package:orangenote/services/auth/bloc/auth_bloc.dart';
+import 'package:orangenote/services/auth/bloc/auth_event.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -23,16 +26,18 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
           const Text(
               "If you haven't received a verification email yet, press the button below"),
           TextButton(
-            onPressed: () async {
-              AuthService.firebase().sendEmailVerification();
+            onPressed: () {
+              context.read<AuthBloc>().add(
+                    const AuthEventSendEmailVerification(),
+                  );
             },
             child: const Text('Send Email Verification'),
           ),
           TextButton(
               onPressed: () async {
-                await AuthService.firebase().logOut();
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(registerRoute, (route) => false);
+                context.read<AuthBloc>().add(
+                      const AuthEventLogOut(),
+                    );
               },
               child: const Text('Restart'))
         ],
